ab8eafbf4f73995b131d7f8ff25582cf64f69c9c
gilbert1412 <gilbertjesusmg@gmail.com>
2026-07-14 22:04:44 -0500

    Se restringieron las rutas se debe estar autenticado para ingresar

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('Admin/login');
-});
+})->name('login');
 
+Route::get('/login', function () {
+    return view('Admin.login');
+})->name('login');
+
+Route::middleware(['auth'])->group(function () {
 Route::get('/usuario-registro',[UsuarioController::class,'index'])->name('usuario');
 Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+});
+
+
+
+
+
+
 
 Route::post('/logout', function () {
 
